b284d9f805d3b2730ce7b0ff4d1fb9367b29597d
unitoshka <unitoshka77@gmail.com>
2024-08-04 18:08:35 +0500

    added modes

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,45 @@ fn main() {
     println!("\x1b[93mWelcome to the KP CLI\x1b[0m");
 
     let stdin = io::stdin();
-    let re = Regex::new(r"^(https?:///*)?([a-z\d-]+\.[a-z\d]+)([a-z\d-]+\.)*/?(\S+)?$").unwrap();
+    println!("Patch Mode (1 - Regex validate mode, 2 - Dev Mode (localhost, 127.0.0.1 and etc)): ");
+    let mut mode = String::new();
+
+    stdin.read_line(&mut mode);
+    let mode = mode.trim().parse::<u8>().unwrap();
+
+    let mut url = String::from("");
 
     println!("Enter URL: ");
     let mut input = String::new();
     
     stdin.read_line(&mut input);
 
-    let mut url = String::from("");
+    if(mode == 1) {
+        let re = Regex::new(r"^(https?:///*)?([a-z\d-]+\.[a-z\d]+)([a-z\d-]+\.)*/?(\S+)?$").unwrap();
 
-    // bruh moment but &input without trim function doesnt work
-    if let Some(caps) = re.captures(&input.trim()) {
-        if let Some(domain) = caps.get(2) {
-            url = get_url(domain);
+        // bruh moment but &input without trim function doesnt work
+        if let Some(caps) = re.captures(&input.trim()) {
+            if let Some(domain) = caps.get(2) {
+                url = get_url(domain.as_str(), mode);
+            }
+        } else {
+            panic!("Invalid URL");
         }
+        modify_gd(&url.as_str());
     } else {
-        panic!("Invalid URL");
-    }
+        url = get_url(&input.trim(), mode);
 
-    modify_gd(&url.as_str());
+        modify_gd(&url.as_str());
+    }
 }
 
-fn get_url(domain: Match) -> String {
-    let mut url = format!("https://{}/", domain.as_str());
+fn get_url(domain: &str, mode: u8) -> String {
+    let protocol = "http";
+    if mode == 1 {
+        protocol.to_owned().push_str("s")
+    }
+
+    let mut url = format!("{protocol}://{domain}/");
     let url_chars = url.chars().count();
 
     if url_chars < 26 {
